0cf0aec1bddeaf4ab1eb73dc94dac61a5f4c8ff5
luckymogale64 <57522837+luckymogale64@users.noreply.github.com>
2021-11-15 15:34:32 +0200

    fixed total add

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 function Vote() {
-  //policital party variable
-  let anc = 0;
-  let da = 0;
-  let eff = 0;
-  let ifp = 0;
-  let actionsa = 0;
-  let icosa = 0;
 
+ //policital party variable
+ let anc = 0;
+ let da = 0;
+ let eff = 0;
+ let ifp = 0;
+ let actionsa = 0;
+ let icosa = 0;
+ 
   const name = document.getElementById("name").value;
   const age = document.getElementById("age").value;
   const nationality = document.getElementById("nationality").value;
   const party = document.getElementById("party").value;
+
+
+  console.log(nationality)
 
   const ancDisplay = document.getElementById("ancTotal");
   const daDisplay = document.getElementById("daTotal");
@@ -18,10 +22,12 @@ function Vote() {
   const ifpDisplay = document.getElementById("ifpTotal");
   const actionsaDisplay = document.getElementById("actionsaTotal");
   const icosaDisplay = document.getElementById("icosaTotal");
+  
+
 
   alert(party);
 
-  if (name == "") {
+  if (name == " ") {
     alert("Please provide your name");
   }
 
@@ -30,8 +36,14 @@ function Vote() {
     return;
   }
 
-  if ((nationality = "foreiger")) {
+  if (nationality = "Foreiger") {
     alert("only south africa can vote");
+    return;
+  }
+
+  if (party = "ANC") {
+    alert("Only south african can vote");
+    return;
   }
 
   if (party == "ANC") {
@@ -70,3 +82,19 @@ function Vote() {
     return;
   }
 }
+
+ display.innerHTML = '';
+
+ for (let i =0; i < party.length; i++) {
+   if ((party[i].number == number)) {
+     display.innerHTML += "party" + party[i].number + "party";
+   }
+ } 
+
+
+ document.getElementById('anc').innerHTML = anc;
+ document.getElementById('da').innerHTML = da;
+ document.getElementById('eff').innerHTML = eff;
+ document.getElementById('ifp').innerHTML = ifp;
+ document.getElementById('actionsa').innerHTML = actionsa;
+ document.getElementById('icosa').innerHTML = icosa;
